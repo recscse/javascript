87f9d516d16d7f90e2bcc78b20c8ca5c97160440
recscse <recs.cse17903@gmail.com>
2021-08-16 22:09:55 +0530

    DOM

---
+++ DOM/chnage the font size color of text/index.js
@@ -0,0 +1,8 @@
+
+
+function jsStyle() {
+    let text = document.getElementById('text')
+     text.style.color = "green";
+     text.style.fontFamily = "Comic Sans MS";
+     text.style.fontSize = "20pt";
+}
